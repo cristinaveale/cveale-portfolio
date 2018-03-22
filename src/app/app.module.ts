87555c14271d064c3bff41039bd4c9578f77e56a
cristinaveale <cristinaveale@gmail.com>
2018-03-22 09:42:32 -0400

    refactoring layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ClarityModule } from '@clr/angular';
-import { UiModule } from './ui/ui.module';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, ClarityModule.forRoot(), UiModule],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ServicesComponent
+  ],
+  imports: [BrowserModule, ClarityModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
