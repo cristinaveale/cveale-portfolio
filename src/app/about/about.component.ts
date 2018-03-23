579467ec7f6b0ed44e9760cf0642b2ab4e134737
cristinaveale <cristinaveale@gmail.com>
2018-03-23 15:32:20 -0400

    Added about me category

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    trigger('movePanel', [
+      transition('void => *', [
+        style({ transform: 'translateY(-100)' }),
+        animate(1.2)
+      ])
+    ])
+  ]
 })
-export class AboutComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+export class AboutComponent {
+  state = 'inactive';
+
+  showPanel() {
+    this.state = this.state === 'inactive' ? 'active' : 'inactive';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ClarityModule } from '@clr/angular';
 
@@ -20,7 +21,7 @@ import { ServicesComponent } from './services/services.component';
     PortfolioComponent,
     ServicesComponent
   ],
-  imports: [BrowserModule, ClarityModule.forRoot()],
+  imports: [BrowserModule, BrowserAnimationsModule, ClarityModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
